879f3ac84625b1c4b5ed8edbcc266a9803ec29d2
Andrii Mishchenko <maloi.promist@gmail.com>
2024-09-10 00:04:40 +0300

    fixed path to templates

--- src/main/java/uk/sevastianov/springmvcapp/controller/HelloController.java
+++ src/main/java/uk/sevastianov/springmvcapp/controller/HelloController.java
@@ -2,12 +2,11 @@ package uk.sevastianov.springmvcapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+
     @GetMapping("/hello-world")
-    @ResponseBody
     public String sayHello() {
         System.out.println("\n----------------");
         return "hello_world";
